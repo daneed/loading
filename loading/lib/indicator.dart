28314b915b1574d5cdf909674fb528c3d5b9dbee
Serly <dserly@graphisoft.com>
2020-01-30 10:46:45 +0100

    +++

--- loading/lib/indicator.dart
+++ loading/lib/indicator.dart
@@ -23,6 +23,8 @@ abstract class Indicator {
   void dispose() {
     if (animationControllers != null) {
       for (var i = 0; i < animationControllers.length; i++) {
+        //print("<flutter_loading plugin> dispose$i called");
+        animationControllers[i].stop();
         animationControllers[i].dispose();
       }
     }
@@ -30,6 +32,7 @@ abstract class Indicator {
 
   void startAnims(List<AnimationController> controllers) {
     for (var i = 0; i < controllers.length; i++) {
+      //print("<flutter_loading plugin> startAnim$i called");
       startAnim(controllers[i]);
     }
   }
